4c4cdf798ca2e8b3c3228961bce750ae7414b4ab
breen <jon16017@byui.edu>
2019-06-12 19:23:38 -0600

    Added some of the tables needed (didn't make the database yet)

---
+++ MegaDeskWeb/Models/Desk.cs
@@ -0,0 +1,19 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MegaDeskWeb.Models
+{
+    public class Desk
+    {
+        public int ID { get; set; }
+        public string Customer { get; set; }
+        public int Width { get; set; }
+        public int Depth { get; set; }
+        public int NumDrawers { get; set; }
+        public string Materials { get; set; }
+        public string Delivery { get; set; }
+        
+    }
+}
---
+++ MegaDeskWeb/Models/PriceDesk.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MegaDeskWeb.Models
+{
+    public class PriceDesk
+    {
+        public int ID { get; set; }
+        public int BasePrice { get; set; }
+        public int SerfaceArea { get; set; }
+        public int Drawers { get; set; }
+        public int Oak { get; set; }
+        public int Laminate { get; set; }
+        public int Pine { get; set; }
+        public int Rosewood { get; set; }
+        public int Veneer { get; set; }
+
+
+    }
+}
